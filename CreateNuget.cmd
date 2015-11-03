63001c80dfe942ea3994a5c6f72caba5284d1725
Llewellyn Falco <isidore@setgame.com>
2015-11-03 08:27:08 -0800

    nuget fixes

--- CreateNuget.cmd
+++ CreateNuget.cmd
@@ -1,4 +1,4 @@
-set version=1.0.0
+set version=1.0.1
 if not exist .\nuget_packages mkdir nuget_packages
 del /Q .\nuget_packages\*.*
 .nuget\NuGet.exe pack ApprovalTests.Asp\ApprovalTests.Asp.csproj -OutputDirectory .\nuget_packages -Version %version% -symbols
