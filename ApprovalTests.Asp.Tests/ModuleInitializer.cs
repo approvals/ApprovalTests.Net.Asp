56fa03a6c2cb3bc2030591586eda105bdb2e1770
Llewellyn Falco <llewellyn.falco@gmail.com>
2019-08-19 22:14:52 -0700

    t added clear to allow for approvaltests

---
+++ ApprovalTests.Asp.Tests/ModuleInitializer.cs
@@ -0,0 +1,9 @@
+﻿using ApprovalTests.Namers.StackTraceParsers;
+
+public static class ModuleInitializer
+{
+    public static void Initialize()
+    {
+        AttributeStackTraceParser.FileInfoIsValidFilter = caller => true;
+    }
+}
