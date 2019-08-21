329d9c9da81610c046fb537e8b4ad6a25178abe2
Simon Cropp <simon.cropp@gmail.com>
2019-08-22 09:31:39 +1000

    *** not workin state

--- MvcApplication.Razor/Controllers/ExampleController.cs
+++ MvcApplication.Razor/Controllers/ExampleController.cs
@@ -5,21 +5,21 @@ using MvcApplication1.Models;
 
 namespace MvcApplication1.Controllers
 {
-    public class ExampleController : ControllerWithExplicitViews // Automatically adds the .Explicit to all views 
+    public class ExampleController : ControllerWithExplicitViews // Automatically adds the .Explicit to all views
     {
         public ActionResult Index()
         {
             return View().Explicit();
             /*
-             * .Explicit() 
-             * is the same as 
+             * .Explicit()
+             * is the same as
              * View("Index");
              * and allows the view to work when called from the test controller
-             * 
+             *
              * Note: this is not actually needed if you use the ControllerWithExplicitViews as a base class
              */
         }
-        
+
         public ActionResult PageWithVerificationToken()
         {
             return View().Explicit();
