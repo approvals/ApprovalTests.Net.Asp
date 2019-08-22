8832019b7d2df8a5b88e64b8f12e15b8cbf1079f
Llewellyn Falco <llewellyn.falco@gmail.com>
2019-08-22 12:40:25 -0700

    e updated nuget scripts

--- CreateNuget.cmd
+++
@@ -1,5 +0,0 @@
-set version=1.1.0
-if not exist .\nuget_packages mkdir nuget_packages
-del /Q .\nuget_packages\*.*
-.nuget\NuGet.exe pack ApprovalTests.Asp\ApprovalTests.Asp.csproj -OutputDirectory .\nuget_packages -Version %version% -symbols
-pause 
